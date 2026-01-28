0d96b2e377db203ab275456aef778013d0dbc471
IKT <kit311290@gmail.com>
2026-01-28 14:50:28 +0300

    cyclic enemies spawn

--- Assets/VampireSurvivorsClone/Code/Units/Enemy/EnemySpawner.cs
+++ Assets/VampireSurvivorsClone/Code/Units/Enemy/EnemySpawner.cs
@@ -3,20 +3,44 @@ using Zenject;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private float _rateOfSpawn = 0.5f;
+    [SerializeField] private float _spawnRadius = 40f;
+
     private EnemyFactory _enemyFactory;
+    private Transform _hero;
+
+    private float _nextSpawnTime;
 
     [Inject]
-    private void Construct(EnemyFactory enemyFactory)
+    private void Construct(EnemyFactory enemyFactory, Hero hero)
     {
         _enemyFactory = enemyFactory;
+        _hero = hero.transform;
     }
 
+    private void Update() => 
+        TrySpawn();
 
-    private void Awake()
+    private void TrySpawn()//AI
     {
-        _enemyFactory.SpawnEnemy(new Vector3(-26, 0, -6));
-        _enemyFactory.SpawnEnemy(new Vector3(14, 0, -14));
-        _enemyFactory.SpawnEnemy(new Vector3(25, 0, 16));
-        _enemyFactory.SpawnEnemy(new Vector3(-12, 0, 6));
+        float interval = 1f / _rateOfSpawn;
+
+        if (Time.time < _nextSpawnTime)
+            return;
+
+        _nextSpawnTime = Time.time + interval;
+
+        _enemyFactory.SpawnEnemy(GetSpawnPosition());
+    }
+
+    private Vector3 GetSpawnPosition() //AI
+    {
+        Vector2 circle = Random.insideUnitCircle.normalized * _spawnRadius;
+
+        return new Vector3(
+            _hero.position.x + circle.x,
+            0f,
+            _hero.position.z + circle.y
+        );
     }
 }
